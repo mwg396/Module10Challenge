23681eb28a012b8a79da4a26ba3d77626a65f4c7
mwg396 <153116787+mwg396@users.noreply.github.com>
2024-01-25 20:55:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const {Triangle, Circle, Square} = require("./lib/shapes.js");
+const {Triangle, Circle, Square} = require("./Develop/lib/shapes.js");
 
 class Svg{
     constructor(){
